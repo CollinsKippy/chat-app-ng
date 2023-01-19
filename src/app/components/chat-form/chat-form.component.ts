e6517b0487a79280923e1528e3b341eb2c9084bb
CK <collins@analence.com>
2023-01-19 21:51:10 +0300

    added chat form and list

--- src/app/components/chat-form/chat-form.component.ts
+++ src/app/components/chat-form/chat-form.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-chat-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+  ],
   templateUrl: './chat-form.component.html',
-  styleUrls: ['./chat-form.component.scss']
+  styleUrls: ['./chat-form.component.scss'],
 })
 export class ChatFormComponent {
+  chatForm = new FormGroup({
+    message: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(2)],
+      asyncValidators: [],
+    }),
+  });
 
+  get message() {
+    return this.chatForm.get('message');
+  }
+
+  onSubmit() {
+    throw new Error('Method not implemented.');
+  }
 }
--- src/app/components/chat-list/chat-list.component.ts
+++ src/app/components/chat-list/chat-list.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+
+type Chat = {
+  id: string;
+  message: string;
+  createdAt: Date;
+};
 
 @Component({
   selector: 'app-chat-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatListModule, MatIconModule],
   templateUrl: './chat-list.component.html',
-  styleUrls: ['./chat-list.component.scss']
+  styleUrls: ['./chat-list.component.scss'],
 })
 export class ChatListComponent {
-
+  chats: Chat[] = [
+    { id: '1', message: 'Hi there, John!', createdAt: new Date() },
+    { id: '2', message: 'Hi there, Jill!', createdAt: new Date() },
+    { id: '3', message: 'Hi there, Max!', createdAt: new Date() },
+  ];
 }
