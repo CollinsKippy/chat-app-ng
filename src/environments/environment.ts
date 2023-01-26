9b6670a6f10c285a7a3fa25332000efc446c0e07
CK <collins@analence.com>
2023-01-26 16:25:06 +0300

    updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   firebase: {
-    projectId: 'chat-app-3dc3c',
-    appId: '1:446444474440:web:f5f3d912faee2ac892087d',
-    storageBucket: 'chat-app-3dc3c.appspot.com',
-    apiKey: 'AIzaSyASRHmutpNE_ccRUU5LW_YhQhYP8uw-kJ4',
-    authDomain: 'chat-app-3dc3c.firebaseapp.com',
-    messagingSenderId: '446444474440',
+    projectId: '',
+    appId: '',
+    storageBucket: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
   },};
